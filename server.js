40f014ebdbb2ddbfdc59117c49db3d8acc774d4f
nathanbirchfield123-ops <nathanbirchfield123@gmail.com>
2026-04-05 14:50:24 -0400

    Initial commit: Add backend server files

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const mysql = require("mysql2")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+
+const db = mysql.createConnection({
+  host: "3.214.216.195",
+  user: "webuser",
+  password: "Password1!",
+  database: "medicalportal"
+})
+
+app.get("/", (req, res) => {
+  res.send("Backend is running")
+})
+
+app.get("/records", (req, res) => {
+  db.query("SELECT * FROM records", (err, results) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ error: "Database query failed", details: err.message })
+    } else {
+      res.json(results)
+    }
+  })
+})
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
+});
